8553f3fd379f2ff494f509a38c03a2dbfe826d32
Greg <glindeman23@gmail.com>
2022-06-03 09:09:08 -0400

    adding portfolio example, and css examples

---
+++ portfolio/js/app.js
@@ -0,0 +1,12 @@
+console.log('hello world')
+
+// alert('help!!!!');
+
+function test(thisIsATest) {
+    const fun = true
+    const statement = 'This is a fun string'
+    return thisIsATest ? console.log('yay') : console.log('booo')
+}
+
+
+test(true)
